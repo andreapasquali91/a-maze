8cecf1fafa1ac51a859259011131e1713127f55f
Andrea Pasquali <51610619+andreapasquali91@users.noreply.github.com>
2021-11-19 12:05:37 +0100

    Update aMaze.py

--- aMaze.py
+++ aMaze.py
@@ -1,6 +1,6 @@
 #This program generates and draws a rectangular maze, with an entrance on one side and one on the opposite side.
-#The walls have two connected components, and there is one (and only one) way from one entrance to the other.
-#To run the program, change "rows" and "columns" as desired inside main(), and interpret using python3.
+#The walls have two connected components, and there is one (and only one) way from one entrance to the other. 
+#To run the program, change "rows" and "columns" as desired inside main(), and interpret using python3. 
 #Due to how matplotlib coordinates work, rows and columns may mean the opposite of what you think.
 
 import matplotlib.pyplot as plt
@@ -9,18 +9,24 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 
-#generate the table. initially every side node is 1 (has a wall) and every inner node is 0 (does not have a wall)
+#generate the table. initially every side node is 1 (has a wall) and every inner node is 0 (does not have a wall).
+#also generates the list of "full" nodes.
 def GenTable(rows,columns):
+	fullList=[]
 	table = [[0] * columns for i in range(rows)]
 	for i in range(rows):
 		table[i][0]=1
 		table[i][columns-1]=1
+		fullList.append([i,0])
+		fullList.append([i,columns-1])
 	for j in range(columns):		
 		table[0][j]=1
 		table[rows-1][j]=1
-	return table
+		fullList.append([0,j])
+		fullList.append([rows-1,j])
+	return table,fullList
 		
-#plot the sides of the rectangle minus two random segments, and create the figure object
+#plot the sides of the rectangle minus two random segments, and create the figure object.
 def DrawSideWalls(rows, columns):
 	plt.figure(figsize=[20,20])
 	x=[0,0]
@@ -48,7 +54,8 @@ def DrawSideWalls(rows, columns):
 	plt.gca().axes.get_xaxis().set_visible(False)
 	plt.gca().axes.get_yaxis().set_visible(False)
 
-#given a node, find a random "empty" node which is adjacent. Empty will mean that no wall ends there.
+#given a node, find a random "empty" node which is adjacent. Empty means that no wall ends there.
+#returns False if all 4 neighbours are full.
 def FindEmptyNeighbour(node,table,rows,columns):
 	directions = ['N','S','E','W']
 	random.shuffle(directions)
@@ -63,34 +70,38 @@ def FindEmptyNeighbour(node,table,rows,columns):
 			return [node[0]-1,node[1]]
 	return False
 
-#main loop. We keep a list of all the nodes which are still empty, and keep adding walls connecting a random non-empty node to a random empty neighbour.
-def Fill(table,rows,columns):
+#main loop. We keep adding walls connecting a random non-empty node to a random empty neighbour.
+#We keep a list of noeds which have a wall (to pick at random from), and remove from it the nodes which have no empty neighbours.
+def Fill(table,rows,columns,fullList):
 	counter=0
+	#total number of walls added will be
 	while counter < (rows-2)*(columns-2):
-		while True:
-			a=random.randint(0,rows-1)
-			b=random.randint(0,columns-1)
-			if table[a][b]==1:
-				node=[a,b]
-				break
+		#choose a random full node
+		index = random.randint(0,len(fullList)-1)
+		node = fullList[index]
 		while True:
 			nei = FindEmptyNeighbour(node,table,rows,columns)
 			if nei!=False:
 				x=[node[0],nei[0]]
 				y=[node[1],nei[1]]
 				plt.plot(x,y, c='black', linewidth=.5)
+				#now the endpoint of the new wall is the new node we start from
 				table[nei[0]][nei[1]] = 1
+				fullList.append(nei)
 				counter=counter+1
 				node = nei
-			else:break
+			else:
+				#remove node from full list if it has no empty neighbours
+				fullList[-1],fullList[index]=fullList[index],fullList[-1]
+				fullList.pop()
+				break
 
 def main():
 	columns = 200
 	rows = 150
-	table=GenTable(rows,columns)
+	table,fullList=GenTable(rows,columns)
 	DrawSideWalls(rows, columns)
-	Fill(table,rows,columns)
+	Fill(table,rows,columns,fullList)
 	plt.savefig(str(columns)+"x"+str(rows)+"_"+datetime.now().strftime("%H%M%S"), bbox_inches='tight')
-	plt.show()
 
 main()
